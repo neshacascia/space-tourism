1abf18ec37cc67dd7de28f045804484153b54dd0
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-19 02:02:15 -0500

    stored destination nav links in an array to map over

--- src/components/navbars/DestinationNavbar.jsx
+++ src/components/navbars/DestinationNavbar.jsx
@@ -1,44 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { planetsNavBarArr } from '../../navbarData';
+
 export default function DestinationNavbar() {
+  const planetsNavLinks = planetsNavBarArr.map(planet => (
+    <li>
+      <NavLink
+        to={planet.path}
+        className="hover:border-b-4 hover:border-gray focus:border-b-4 active:border-gray pb-2"
+        key={planet.id}
+      >
+        {planet.name}
+      </NavLink>
+    </li>
+  ));
+
   return (
     <ul className="text-lightBlue font-h2 text-base font-normal leading-5 tracking-wider flex gap-6 items-center justify-center mb-8 lg:justify-start">
-      <li>
-        <NavLink
-          to="/destination/moon"
-          className="hover:border-b-4 hover:border-gray focus:border-b-4 pb-2"
-        >
-          MOON
-        </NavLink>
-      </li>
-
-      <li>
-        <NavLink
-          to="/destination/mars"
-          className="hover:border-b-4 hover:border-gray focus:border-b-4 pb-2"
-        >
-          MARS
-        </NavLink>
-      </li>
-
-      <li>
-        <NavLink
-          to="/destination/europa"
-          className="hover:border-b-4 hover:border-gray focus:border-b-4 pb-2"
-        >
-          EUROPA
-        </NavLink>
-      </li>
-
-      <li>
-        <NavLink
-          to="/destination/titan"
-          className="hover:border-b-4 hover:border-gray focus:border-b-4 pb-2"
-        >
-          TITAN
-        </NavLink>
-      </li>
+      {planetsNavLinks}
     </ul>
   );
 }
--- src/navbarData.js
+++ src/navbarData.js
@@ -24,3 +24,26 @@ export const navBarArr = [
     name: 'TECHNOLOGY',
   },
 ];
+
+export const planetsNavBarArr = [
+  {
+    id: '0',
+    path: '/destination/',
+    name: 'MOON',
+  },
+  {
+    id: '1',
+    path: '/destination/mars',
+    name: 'MARS',
+  },
+  {
+    id: '2',
+    path: '/destination/europa',
+    name: 'EUROPA',
+  },
+  {
+    id: '3',
+    path: '/destination/titan',
+    name: 'TITAN',
+  },
+];
