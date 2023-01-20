df3c382baa5d6d7356f3a81297aee9205d14a556
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-20 01:52:45 -0500

    created separate .js file to store navbar data so i can map over the array

--- src/components/navbars/Navbar.jsx
+++ src/components/navbars/Navbar.jsx
@@ -3,39 +3,28 @@ import { NavLink } from 'react-router-dom';
 import logo from '/assets/shared/logo.svg';
 import hamburgerMenu from '/assets/shared/icon-hamburger.svg';
 import { Context } from '../Context';
+import { navBarArr } from '../../navbarData';
 
 export default function Navbar(props) {
   const { openModal } = useContext(Context);
+
+  const navLinks = navBarArr.map(nav => (
+    <NavLink
+      to={nav.path}
+      className="hover:border-b-4 hover:border-lightGray focus:border-b-4 pb"
+      key={nav.id}
+    >
+      <span className="font-bold pr-3 hidden lg:inline">{nav.num}</span>
+      {nav.name}
+    </NavLink>
+  ));
 
   return (
     <nav className="flex justify-between items-center h-24 pl-6 lg:pt-10">
       <img src={logo} className="w-10 h-10 lg:ml-14 lg:mr-14" />
       <hr className="w-navBorder border border-gray hidden lg:inline" />
       <ul className="hidden sm:text-white sm:font-h2 tracking-widest sm:flex sm:justify-center sm:items-center sm:gap-8 sm:w-tabletNav sm:h-tabletNav sm:bg-mobileMenu sm:backdrop-blur-mobileMenu lg:w-desktopNav lg:justify-start lg:pl-28 lg:gap-12">
-        <NavLink
-          to="/"
-          className="hover:border-b-4 hover:border-lightGray focus:border-b-4 pb"
-        >
-          <span className="font-bold pr-3 hidden lg:inline">00</span>HOME
-        </NavLink>
-        <NavLink
-          to="/destination"
-          className="hover:border-b-4 hover:border-lightGray focus:border-b-4 pb"
-        >
-          <span className="font-bold pr-3 hidden lg:inline">01</span>DESTINATION
-        </NavLink>
-        <NavLink
-          to="/crew"
-          className="hover:border-b-4 hover:border-lightGray focus:border-b-4 pb"
-        >
-          <span className="font-bold pr-3 hidden lg:inline">02</span>CREW
-        </NavLink>
-        <NavLink
-          to="/technology"
-          className="hover:border-b-4 hover:border-lightGray focus:border-b-4 pb"
-        >
-          <span className="font-bold pr-3 hidden lg:inline">03</span>TECHNOLOGY
-        </NavLink>
+        {navLinks}
       </ul>
 
       <button onClick={openModal} className="pr-6 sm:hidden">
---
+++ src/navbarData.js
@@ -0,0 +1,26 @@
+export const navBarArr = [
+  {
+    id: '0',
+    path: '/',
+    num: '00',
+    name: 'HOME',
+  },
+  {
+    id: '1',
+    path: '/destination',
+    num: '01',
+    name: 'DESTINATION',
+  },
+  {
+    id: '2',
+    path: '/crew',
+    num: '02',
+    name: 'CREW',
+  },
+  {
+    id: '3',
+    path: '/technology',
+    num: '03',
+    name: 'TECHNOLOGY',
+  },
+];
